6d81b5157251884e1266eb112787f43df8cfc755
Matthew Drury <matthew.drury.83@gmail.com>
2019-12-06 19:35:16 -0800

    Get old tests to run in new intcode implementation.

--- 5/thermal_environment_supervision_terminal.py
+++ 5/thermal_environment_supervision_terminal.py
@@ -9,7 +9,7 @@ INPUT_OPCODE = 3
 OUTPUT_OPCODE = 4
 HALT_OP_CODE = 99
 
-def run(program: List[int]) -> None:
+def run(program: List[int]) -> List[int]:
     instruction_ptr = 0
     halt = False
     while not halt:
@@ -18,13 +18,11 @@ def run(program: List[int]) -> None:
         operation, n_parameters = OP_CODE_TABLE[opcode]
         if len(parameter_modes) != n_parameters:
             parameter_modes = parameter_modes + [0]*(n_parameters - len(parameter_modes))
-        raw_parameters = program[instruction_ptr + 1 : instruction_ptr + n_parameters + 1]
-        parameters = [
-            lookup_parameter(program, parameter, mode)
-            for parameter, mode in zip(raw_parameters, parameter_modes)
-        ]
-        program, halt = operation(program, parameters, instruction_ptr)
-    return None
+        parameters = program[instruction_ptr + 1 : instruction_ptr + n_parameters + 1]
+        program, halt = operation(
+            program, parameters, parameter_modes, instruction_ptr)
+        instruction_ptr += n_parameters + 1
+    return program
 
 def parse_opcode(full_opcode: int) -> Tuple[int, List[int]]:
     s = ''.join(reversed(str(full_opcode)))
@@ -43,22 +41,33 @@ def lookup_parameter(program: List[int], parameter: int, mode: int) -> int:
 def add(
     program: List[int], 
     parameters: List[int], 
+    parameter_modes: List[int], 
     current_position: int) -> Tuple[List[int], bool]:
-    a1, a2, address = parameters
+    a1, a2 = [
+        lookup_parameter(program, parameter, mode)
+        for parameter, mode in zip(parameters[:2], parameter_modes[:2])
+    ]
+    address = parameters[2]
     program[address] = a1 + a2
     return program, False
 
 def multiply(
     program: List[int], 
     parameters: List[int], 
+    parameter_modes: List[int], 
     current_position: int) -> Tuple[List[int], bool]:
-    m1, m2, address = parameters
+    m1, m2 = [
+        lookup_parameter(program, parameter, mode)
+        for parameter, mode in zip(parameters[:2], parameter_modes[:2])
+    ]
+    address = parameters[2]
     program[address] = m1 * m2
     return program, False
 
 def input_(
     program: List[int], 
     parameters: List[int], 
+    parameter_modes: List[int], 
     current_position: int) -> Tuple[List[int], bool]:
     val = int(input('> '))
     program[parameters[0]] = val
@@ -67,6 +76,7 @@ def input_(
 def output(
     program: List[int], 
     parameters: List[int], 
+    parameter_modes: List[int], 
     current_position: int) -> Tuple[List[int], bool]:
     print(program[parameters[0]])
     return program, False
@@ -74,6 +84,7 @@ def output(
 def halt(
     program: List[int], 
     parameters: List[int], 
+    parameter_modes: List[int], 
     current_position: int) -> Tuple[List[int], bool]:
     return program, True
 
@@ -85,3 +96,10 @@ OP_CODE_TABLE = {
     OUTPUT_OPCODE: (output, 1),
     HALT_OP_CODE: (halt, 0)
 }
+
+
+assert run([1,0,0,0,99]) == [2,0,0,0,99]
+assert run([2,3,0,3,99]) == [2,3,0,6,99]
+assert run([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+assert run([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+assert run([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
