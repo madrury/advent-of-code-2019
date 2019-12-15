ab0d1973cf09da5f2966b17973927039f24c2697
Matthew Drury <matthew.drury.83@gmail.com>
2019-12-14 17:09:40 -0800

    Finish laser rotation problem.
    Inverted y-axies are bullshit.

--- 10/asteroid_detector.py
+++ 10/asteroid_detector.py
@@ -1,5 +1,6 @@
-from typing import IO, List, Tuple, Set
-from math import gcd
+from typing import IO, List, Tuple, Set, Iterable
+from math import gcd, atan2, pi
+from collections import defaultdict
 
 
 Chart = List[List[int]]
@@ -46,22 +47,68 @@ def find_best_station_position(chart: Chart) -> Tuple[Point, int]:
         candidates[position] = len(get_asteroids_visible_from(position, positions)) - 1
     return max(candidates.items(), key=lambda item: item[1])
 
+def angle_to_vertical(p: Point, reference: Point) -> float:
+    theta = atan2(*(p[0] - reference[0], p[1] - reference[1]))
+    return - theta
 
-chart1 = read_asteroid_chart(open('./data/chart-1.txt', 'r'))
-assert find_best_station_position(chart1) == ((3, 4), 8)
+def reduce_point(p: Point, reference: Point):
+    (x, y), (r1, r2) = p, reference
+    g = gcd(x - r1, y - r2)
+    return ((x - r1) // g, (y - r2) // g)
 
-chart2 = read_asteroid_chart(open('./data/chart-2.txt', 'r'))
-assert find_best_station_position(chart2) == ((5, 8), 33)
+def group_asteroids_by_angle(positions: Set[Point], reference: Point):
+    groups = defaultdict(list)
+    for position in positions:
+        groups[reduce_point(position, reference)].append(position)
+    return groups
 
-chart3 = read_asteroid_chart(open('./data/chart-3.txt', 'r'))
-assert find_best_station_position(chart3) == ((1, 2), 35)
+def sort_asteroid_groups_by_angle(positions: Set[Point], reference: Point) -> List[List[Point]]:
+    groups = group_asteroids_by_angle(positions, reference)
+    s = sorted(groups.items(), key=lambda item: angle_to_vertical(item[1][0], reference))
+    norm = lambda p: (p[0] - reference[0])**2 + (p[1] - reference[1])**2
+    return [list(sorted(x[1], key=norm)) for x in s]
 
-chart4 = read_asteroid_chart(open('./data/chart-4.txt', 'r'))
-assert find_best_station_position(chart4) == ((6, 3), 41)
+def iter_asteroids_by_angle(positions: Set[Point], reference: Point) -> Iterable[Point]:
+    # positions = chart
+    positions.remove(reference)
+    s = sort_asteroid_groups_by_angle(positions, reference)
+    while not all(group == [] for group in s):
+        for group in s:
+            if group:
+                yield group.pop(0)
 
-chart5 = read_asteroid_chart(open('./data/chart-5.txt', 'r'))
-assert find_best_station_position(chart5) == ((11, 13), 210)
 
-chart = read_asteroid_chart(open('./data/chart.txt', 'r'))
-location, n_visible = find_best_station_position(chart)
-print(f"Best space station location is {location} with {n_visible} visible asteroids")
+
+if __name__ == '__main__':
+    chart1 = read_asteroid_chart(open('./data/chart-1.txt', 'r'))
+    assert find_best_station_position(chart1) == ((3, 4), 8)
+
+    chart2 = read_asteroid_chart(open('./data/chart-2.txt', 'r'))
+    assert find_best_station_position(chart2) == ((5, 8), 33)
+
+    chart3 = read_asteroid_chart(open('./data/chart-3.txt', 'r'))
+    assert find_best_station_position(chart3) == ((1, 2), 35)
+
+    chart4 = read_asteroid_chart(open('./data/chart-4.txt', 'r'))
+    assert find_best_station_position(chart4) == ((6, 3), 41)
+
+    chart5 = read_asteroid_chart(open('./data/chart-5.txt', 'r'))
+    assert find_best_station_position(chart5) == ((11, 13), 210)
+
+    chart = read_asteroid_chart(open('./data/chart.txt', 'r'))
+    location, n_visible = find_best_station_position(chart)
+    print(f"Best space station location is {location} with {n_visible} visible asteroids")
+
+    # positions = get_asteroid_positions(chart5)
+    # order_destroyed = list(iter_asteroids_by_angle(positions, (11, 13)))
+    # print(order_destroyed)
+    # print(f"The 200th asteroid to be destroyed is {order_destroyed[199]}")
+
+    # chart_simple = read_asteroid_chart(open('./data/chart-simple.txt', 'r'))
+    # positions = get_asteroid_positions(chart_simple)
+    # order_destroyed = list(iter_asteroids_by_angle(positions, (2, 2)))
+    # print(order_destroyed)
+
+    positions = get_asteroid_positions(chart)
+    order_destroyed = list(iter_asteroids_by_angle(positions, (37, 25)))
+    print(f"The 200th asteroid to be destroyed is {order_destroyed[199]}")
