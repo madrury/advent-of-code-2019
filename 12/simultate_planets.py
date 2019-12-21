5e30d0283bac3dee4161410a751cdb611a7c4641
Matthew Drury <matthew.drury.83@gmail.com>
2019-12-20 21:14:30 -0800

    Finish day 12.

--- 12/simultate_planets.py
+++ 12/simultate_planets.py
@@ -1,12 +1,29 @@
 from typing import Tuple, List
+from itertools import combinations
+import math
+import functools
 import re
+
+# TIL: Python has no built in sign.
+sign = lambda x: 0 if x == 0 else math.copysign(1, x)
 
 
 class Planet:
 
     def __init__(self, position: Tuple[int, int, int]):
         self.position = position
-        self.valocity: Tuple[int, int, int] = (0, 0, 0)
+        self.velocity: Tuple[int, int, int] = (0, 0, 0)
+    
+    def update(self):
+        self.position = tuple(x + v for x, v in zip(self.position, self.velocity))
+    
+    def apply_gravity(self, other: 'Planet'):
+        self.velocity = tuple(
+            v + sign(y - x) for v, x, y in zip(self.velocity, self.position, other.position)
+        )
+    
+    def energy(self):
+        return sum(abs(x) for x in self.position) * sum(abs(x) for x in self.velocity)
     
     @classmethod
     def from_string(cls, s: str) -> 'Planet':
@@ -15,7 +32,44 @@ class Planet:
         return cls(positions)
     
     def __str__(self):
-        return f'Planet{self.position}'
+        return f'Planet{self.position}, v={self.velocity}'
 
-s = '<x=2, y=  5, z= -22>'
-print(Planet.from_string(s))
+
+def simulate(planets: List[Planet], n_steps: int=100) -> List[Planet]:
+    for _ in range(n_steps):
+        for p1, p2 in combinations(planets, r=2):
+            p1.apply_gravity(p2)
+            p2.apply_gravity(p1)
+        for p in planets:
+            p.update()
+
+def find_period_for_axis(planets: List[Planet], axis: int) -> int:
+    n = 0
+    past = {}
+    while True:
+        state = tuple((p.position[axis], p.velocity[axis]) for p in planets)
+        if state in past:
+            return n
+        past[state] = n
+        for p1, p2 in combinations(planets, r=2):
+            p1.apply_gravity(p2)
+            p2.apply_gravity(p1)
+        for p in planets:
+            p.update()
+        n += 1
+        
+
+
+s = open('./data/planets.txt', 'r')
+planets = [Planet.from_string(row.strip()) for row in s]
+simulate(planets, n_steps=1000)
+print(f"The total enery is {sum(p.energy() for p in planets)}")
+
+
+s = open('./data/planets.txt', 'r')
+planets = [Planet.from_string(row.strip()) for row in s]
+periods = tuple(find_period_for_axis(planets, i) for i in range(3))
+
+lcm = lambda a, b: a * b // math.gcd(a, b)
+
+print(f"The period of repition of the planetary system is {functools.reduce(lcm, periods)}")
