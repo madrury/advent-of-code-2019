e60292f6b985c32aff5116ce9a3babc4cc0def1c
Matthew Drury <matthew.drury.83@gmail.com>
2019-12-22 20:36:16 -0800

    Finish day 14 part b.

--- 14/stoichiometry.py
+++ 14/stoichiometry.py
@@ -25,8 +25,8 @@ def parse_line(line: str) -> Reaction:
         parsed_matches.append((chem, int(n)))
     return parsed_matches[-1][0], (parsed_matches[-1][1], parsed_matches[:-1]) 
 
-def ore_needed_for_fuel(table: ReactionTable) -> int:
-    need = {'FUEL': 1}
+def ore_needed_for_fuel(table: ReactionTable, n_fuel: int=1) -> int:
+    need = {'FUEL': n_fuel}
     reserves = {}
     while set(need.keys()) != {'ORE'}:
         new_need = {}
@@ -41,24 +41,43 @@ def ore_needed_for_fuel(table: ReactionTable) -> int:
             for reactant, n_reactant in reactants:
                 new_need[reactant] = new_need.get(reactant, 0) + n_reactions_needed * n_reactant
         need = new_need
-    return need.get('ORE')
+    return need['ORE']
 
+def amount_of_fuel_for_ore(table: ReactionTable, n_ore: int=1_000_000_000_000) -> int:
+    from time import sleep
+    low, high = 0, n_ore
+    while low != high:
+        try_n_fuel = (low + high + 1) // 2
+        ore_needed = ore_needed_for_fuel(table, try_n_fuel)
+        if ore_needed < n_ore:
+            low = try_n_fuel
+        elif ore_needed > n_ore:
+            high = try_n_fuel - 1
+        else:
+            return try_n_fuel
+    return low
 
+# 82892754 82892753
 
-table = read_reaction_file(open('./data/example1.txt', 'r'))
-assert ore_needed_for_fuel(table) == 31 
+# table = read_reaction_file(open('./data/example1.txt', 'r'))
+# assert ore_needed_for_fuel(table) == 31 
 
-table = read_reaction_file(open('./data/example2.txt', 'r'))
-assert ore_needed_for_fuel(table) == 165 
+# table = read_reaction_file(open('./data/example2.txt', 'r'))
+# assert ore_needed_for_fuel(table) == 165 
+
 
 table = read_reaction_file(open('./data/example3.txt', 'r'))
 assert ore_needed_for_fuel(table) == 13312 
+assert amount_of_fuel_for_ore(table, n_ore=1_000_000_000_000) == 82892753
 
 table = read_reaction_file(open('./data/example4.txt', 'r'))
 assert ore_needed_for_fuel(table) == 180697 
+assert amount_of_fuel_for_ore(table, n_ore=1_000_000_000_000) == 5586022
 
 table = read_reaction_file(open('./data/example5.txt', 'r'))
 assert ore_needed_for_fuel(table) == 2210736 
+assert amount_of_fuel_for_ore(table, n_ore=1_000_000_000_000) == 460664
 
 table = read_reaction_file(open('./data/reactions.txt', 'r'))
 print(f"The ore needed to make a FUEL is {ore_needed_for_fuel(table)}")
+print(f"The fuel makable for 1trillion ORE is {amount_of_fuel_for_ore(table)}")
