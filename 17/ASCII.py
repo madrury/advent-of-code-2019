f105ab1aec52205a7183ae3d9b6589c544c58d8e
Matthew Drury <matthew.drury.83@gmail.com>
2019-12-28 20:19:00 -0800

    ASCII robot works.

--- 17/ASCII.py
+++ 17/ASCII.py
@@ -1,4 +1,4 @@
-from intcode.intcode import Program, run_until_output, run
+from intcode.intcode import Program, run_until_matches, run
 from intcode.util import draw_array
 import numpy as np
 
@@ -31,11 +31,50 @@ def iter_intersections(console: np.array) -> Iterable[Point]:
         if np.all(console[x-1:x+2, y-1:y+2] == INTERSECTION_MASK):
             yield x, y
 
+def run_robot(
+    program: Program, 
+    main_routine: str, 
+    functions: List[str],
+    camera_feed: bool=True) -> None:
+    # Force robot to wakeup
+    program[0] = 2
+    # Supply the main movement routine:
+    run(program) 
+    program.input.extend(to_ascii(main_routine, reverse=True))
+    run(program) 
+    for f in functions:
+        program.input.extend(to_ascii(f, reverse=True))
+        run(program) 
+    if camera_feed:
+        program.input.extend(to_ascii('y', reverse=True))
+    else:
+        program.input.extend(to_ascii('n', reverse=True))
+    program.reset_output()
+    # I'm getting a spurious newline at this point, so clear it out.
+    run_until_matches(program, outseq=to_ascii(''))
+    program.reset_output()
+    
+    halt = False
+    while not halt:
+        _, halt = run_until_matches(program, outseq=to_ascii('\n'))
+        console = to_square(program.output)
+        draw_array(console, conversion_table=CONVERSION_DICT); print()
+        program.reset_output()
+
+
+def to_ascii(code: str, reverse: bool=False) -> List[int]:
+    if reverse:
+        return ([ord(ch) for ch in code] + [LINE_END])[::-1]
+    else:
+        return [ord(ch) for ch in code] + [LINE_END]
+
 
 program = Program.from_file(open('./data/ascii.txt', 'r'))
 
-run(program)
-console_out = to_square(program.output)
+#run(program)
+#console_out = to_square(program.output)
 #draw_array(console_out, conversion_table=CONVERSION_DICT)
-total_alignment_parameter = sum(x * y for x, y in iter_intersections(console_out))
-print(f"The total allignment parameter is {total_alignment_parameter}")
+# total_alignment_parameter = sum(x * y for x, y in iter_intersections(console_out))
+# print(f"The total allignment parameter is {total_alignment_parameter}")
+
+run_robot(program, 'A,A,B,A', ['R,2', '2', '1'])
